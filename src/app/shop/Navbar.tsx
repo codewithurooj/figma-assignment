1e2ebaf6acc4cdaa24f57285bb6f4c69df45a950
codewithurooj <165995978+codewithurooj@users.noreply.github.com>
2025-01-19 23:35:36 +0500

    Update Navbar.tsx

--- src/app/shop/Navbar.tsx
+++ src/app/shop/Navbar.tsx
@@ -7,8 +7,6 @@ import { LuUserRound } from "react-icons/lu";
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
-import SupportPage from '@/components/home/SupportPage';
-
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -27,22 +25,22 @@ const Navbar: React.FC = () => {
         </div>
         <nav className="hidden md:flex space-x-8">
           <Link href="/" passHref>
-            <a className="text-yellow-500">Home</a>
+            <span className="text-yellow-500">Home</span>
           </Link>
           <Link href="/menu" passHref>
-            <a className="text-white">Menu</a>
+            <span className="text-white">Menu</span>
           </Link>
           <Link href="/blog" passHref>
-            <a className="text-white">Blog</a>
+            <span className="text-white">Blog</span>
           </Link>
           <Link href="#" passHref>
-            <a className="text-white">Pages</a>
+            <span className="text-white">Pages</span>
           </Link>
           <Link href="/about" passHref>
-            <a className="text-white">About</a>
+            <span className="text-white">About</span>
           </Link>
           <Link href="/shop" passHref>
-            <a className="text-white">Shop</a>
+            <span className="text-white">Shop</span>
           </Link>
         </nav>
         <div className="hidden md:flex space-x-4">
@@ -59,22 +57,22 @@ const Navbar: React.FC = () => {
       {isOpen && (
         <nav className="md:hidden flex flex-col items-center space-y-4 bg-black w-full py-4">
           <Link href="/" passHref>
-            <a className="text-yellow-500">Home</a>
+            <span className="text-yellow-500">Home</span>
           </Link>
           <Link href="/menu" passHref>
-            <a className="text-white">Menu</a>
+            <span className="text-white">Menu</span>
           </Link>
           <Link href="/blog" passHref>
-            <a className="text-white">Blog</a>
+            <span className="text-white">Blog</span>
           </Link>
           <Link href="#" passHref>
-            <a className="text-white">Pages</a>
+            <span className="text-white">Pages</span>
           </Link>
           <Link href="/about" passHref>
-            <a className="text-white">About</a>
+            <span className="text-white">About</span>
           </Link>
           <Link href="/shop" passHref>
-            <a className="text-white">Shop</a>
+            <span className="text-white">Shop</span>
           </Link>
         </nav>
       )}
@@ -82,21 +80,17 @@ const Navbar: React.FC = () => {
         <div className="relative w-full h-64">
           <Image src="/images/menu-hero.png" alt="Shop Image" layout="fill" objectFit="cover" />
           <div className="absolute inset-0 flex flex-col items-center justify-center">
-            <h2 className="text-2xl font-bold">Sign up Page</h2>
+            <h2 className="text-4xl font-bold">About Us</h2>
             <div className="flex space-x-2 mt-2">
               <Link href="/" passHref>
-                <a className="text-white">Home</a>
+                <span className="text-white">Home</span>
               </Link>
               <span className="text-white">&gt;</span>
               <Link href="#" passHref>
-                <a className="text-yellow-500">Sign Up</a>
+                <span className="text-yellow-500">Sign up</span>
               </Link>
             </div>
           </div>
-        </div>
-        
-        <div className='mt-12'>
-          <SupportPage />
         </div>
       </main>
     </div>
