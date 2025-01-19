888541a0e2573d28cdedcdfdf21ed21cbc7fb8b3
codewithurooj <165995978+codewithurooj@users.noreply.github.com>
2025-01-19 23:18:46 +0500

    Update Navbar.tsx

--- src/app/about/Navbar.tsx
+++ src/app/about/Navbar.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link'; // Import Link from next/link
 import { CiSearch } from 'react-icons/ci';
 import { LuUserRound } from "react-icons/lu";
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 import { GiHamburgerMenu } from 'react-icons/gi';
-
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,12 +24,24 @@ const Navbar: React.FC = () => {
           </h1>
         </div>
         <nav className="hidden md:flex space-x-8">
-          <a href="/" className="text-yellow-500">Home</a>
-          <a href="/menu" className="text-white">Menu</a>
-          <a href="/blog" className="text-white">Blog</a>
-          <a href="#" className="text-white">Pages</a>
-          <a href="/about" className="text-white">About</a>
-          <a href="/shop" className="text-white">Shop</a>
+          <Link href="/" passHref>
+            <a className="text-yellow-500">Home</a>
+          </Link>
+          <Link href="/menu" passHref>
+            <a className="text-white">Menu</a>
+          </Link>
+          <Link href="/blog" passHref>
+            <a className="text-white">Blog</a>
+          </Link>
+          <Link href="#" passHref>
+            <a className="text-white">Pages</a>
+          </Link>
+          <Link href="/about" passHref>
+            <a className="text-white">About</a>
+          </Link>
+          <Link href="/shop" passHref>
+            <a className="text-white">Shop</a>
+          </Link>
         </nav>
         <div className="hidden md:flex space-x-4">
           <CiSearch className='text-white' />
@@ -44,12 +56,24 @@ const Navbar: React.FC = () => {
       </header>
       {isOpen && (
         <nav className="md:hidden flex flex-col items-center space-y-4 bg-black w-full py-4">
-          <a href="/" className="text-yellow-500">Home</a>
-          <a href="/menu" className="text-white">Menu</a>
-          <a href="/blog" className="text-white">Blog</a>
-          <a href="#" className="text-white">Pages</a>
-          <a href="/about" className="text-white">About</a>
-          <a href="/shop" className="text-white">Shop</a>
+          <Link href="/" passHref>
+            <a className="text-yellow-500">Home</a>
+          </Link>
+          <Link href="/menu" passHref>
+            <a className="text-white">Menu</a>
+          </Link>
+          <Link href="/blog" passHref>
+            <a className="text-white">Blog</a>
+          </Link>
+          <Link href="#" passHref>
+            <a className="text-white">Pages</a>
+          </Link>
+          <Link href="/about" passHref>
+            <a className="text-white">About</a>
+          </Link>
+          <Link href="/shop" passHref>
+            <a className="text-white">Shop</a>
+          </Link>
         </nav>
       )}
       <main className="flex flex-col items-center w-full bg-white">
@@ -58,13 +82,16 @@ const Navbar: React.FC = () => {
           <div className="absolute inset-0 flex flex-col items-center justify-center">
             <h2 className="text-4xl font-bold">About Us</h2>
             <div className="flex space-x-2 mt-2">
-              <a href="#" className="text-white">Home</a>
-              <span className="text-white">></span>
-              <a href="#" className="text-yellow-500">About</a>
+              <Link href="/" passHref>
+                <a className="text-white">Home</a>
+              </Link>
+              <span className="text-white">&gt;</span>
+              <Link href="#" passHref>
+                <a className="text-yellow-500">About</a>
+              </Link>
             </div>
           </div>
         </div>
-        
       </main>
     </div>
   );
