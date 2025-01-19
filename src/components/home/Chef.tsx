a07bb81a7f4d6f71b2df639e3180bb65b09220fe
codewithurooj <165995978+codewithurooj@users.noreply.github.com>
2025-01-19 19:11:57 +0500

    Update Chef.tsx

--- src/components/home/Chef.tsx
+++ src/components/home/Chef.tsx
@@ -1,4 +1,3 @@
-// components/Chef.tsx
 import React from 'react';
 import Image from 'next/image';
 
@@ -10,25 +9,12 @@ interface ChefProps {
 
 const Chef: React.FC<ChefProps> = ({ imageSrc, name, position }) => {
   return (
-    <div className="flex flex-col items-center mb-4 ">
+    <div className="flex flex-col items-center mb-4">
       <Image src={imageSrc} alt={name} width={300} height={350} className="rounded-lg shadow-lg" />
-      
+      <h2 className="text-xl font-semibold mt-2">{name}</h2>
+      <p className="text-gray-500">{position}</p>
     </div>
   );
 };
 
 export default Chef;
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
