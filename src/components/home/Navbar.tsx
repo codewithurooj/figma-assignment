4b0434b3a9f687315097f2bf213b52175b2dbcb7
codewithurooj <165995978+codewithurooj@users.noreply.github.com>
2025-01-20 18:34:52 +0500

    Update Navbar.tsx

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -1,77 +1,111 @@
 "use client";
 import React, { useState } from 'react';
-import Image from 'next/image';
-import { CiSearch } from 'react-icons/ci';
-import { LuUserRound } from "react-icons/lu";
-import { HiOutlineShoppingBag } from 'react-icons/hi';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import SignUp from './SignUp';
-import SupportPage from '@/components/home/SupportPage';
+import { BsChevronDown } from 'react-icons/bs';
+import SearchBar from './SearchBar';
+import { PiHandbag } from 'react-icons/pi';
+import Link from 'next/link';
 
 const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isPagesDropdownOpen, setIsPagesDropdownOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+  const togglePagesDropdown = () => {
+    setIsPagesDropdownOpen(!isPagesDropdownOpen);
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center text-white">
-      <header className="w-full flex justify-between items-center py-4 px-8 bg-black">
-        <div className="flex items-center">
-          <h1 className="text-3xl font-bold">
-            <span className="text-white">Food</span>
-            <span className="text-yellow-500">tuck</span>
-          </h1>
+    <div className='bg-black'>
+    <nav className="bg-transparent p-4 ">
+      <div className="container mx-auto flex flex-wrap justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <ul className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
+            <li className="relative">
+              <Link href="/" className="text-white flex items-center">
+                Home
+                <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-2 h-2 bg-yellow-500 rounded-full"></span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/menu" className="text-white flex items-center">
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog" className="text-white flex items-center">
+                Blog
+              </Link>
+            </li>
+            <li className="relative">
+              <button onClick={togglePagesDropdown} className="text-white flex items-center focus:outline-none">
+                Pages <BsChevronDown className="ml-1" />
+              </button>
+              {isPagesDropdownOpen && (
+                <ul className="absolute left-0 mt-2 space-y-2 p-2 bg-white text-black shadow-lg">
+                  <li>
+                    <Link href="/about" className="block px-4 py-2 hover:bg-gray-200">
+                      About Us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/team" className="block px-4 py-2 hover:bg-gray-200">
+                      Team
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/faq" className="block px-4 py-2 hover:bg-gray-200">
+                      FAQ
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/404" className="block px-4 py-2 hover:bg-gray-200">
+                      404 Error
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/signup" className="block px-4 py-2 hover:bg-gray-200">
+                      Sign Up
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/checkout" className="block px-4 py-2 hover:bg-gray-200">
+                      Checkout
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/blog" className="block px-4 py-2 hover:bg-gray-200">
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/shop" className="block px-4 py-2 hover:bg-gray-200">
+                      Shop
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <Link href="/about" className="text-white flex items-center">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/shop" className="text-white flex items-center">
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="text-white flex items-center">
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
-        <nav className="hidden md:flex space-x-8">
-          <span className="text-yellow-500">Home</span>
-          <span className="text-white">Menu</span>
-          <span className="text-white">Blog</span>
-          <span className="text-white">Pages</span>
-          <span className="text-white">About</span>
-          <span className="text-white">Shop</span>
-        </nav>
-        <div className="hidden md:flex space-x-4">
-          <CiSearch className='text-white' />
-          <LuUserRound className='text-white' />
-          <HiOutlineShoppingBag className='text-white' />
+        <div className="hidden sm:flex items-center space-x-2 mt-4 sm:mt-0">
+          <SearchBar />
+          <PiHandbag className="text-white" />
         </div>
-        <div className="md:hidden flex items-center">
-          <button onClick={toggleMenu} className="text-white focus:outline-none">
-            <GiHamburgerMenu />
-          </button>
-        </div>
-      </header>
-      {isOpen && (
-        <nav className="md:hidden flex flex-col items-center space-y-4 bg-black w-full py-4">
-          <span className="text-yellow-500">Home</span>
-          <span className="text-white">Menu</span>
-          <span className="text-white">Blog</span>
-          <span className="text-white">Pages</span>
-          <span className="text-white">About</span>
-          <span className="text-white">Shop</span>
-        </nav>
-      )}
-      <main className="flex flex-col items-center w-full bg-white">
-        <div className="relative w-full h-64">
-          <Image src="/images/menu-hero.png" alt="Shop Image" layout="fill" objectFit="cover" />
-          <div className="absolute inset-0 flex flex-col items-center justify-center">
-            <h2 className="text-2xl font-bold">Sign up Page</h2>
-            <div className="flex space-x-2 mt-2">
-              <span className="text-white">Home</span>
-              <span className="text-white">></span>
-              <span className="text-yellow-500">Sign Up</span>
-            </div>
-          </div>
-        </div>
-        <div className='mt-12'>
-        <SignUp/>
-        </div>
-        <div className='mt-12'>
-            <SupportPage/>
-        </div>
-      </main>
+      </div>
+    </nav>
     </div>
   );
 };
